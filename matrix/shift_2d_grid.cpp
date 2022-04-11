027f713f2f60636e0acfffb8cb3f1d13eb846576
Ashutosh Gupta <Ashutoshg547@gmail.com>
2022-04-12 02:41:37 +0530

    added new file

---
+++ matrix/shift_2d_grid.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int M=1e9+7;
+
+vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k)
+{
+    int m=grid.size();
+    int n=grid[0].size();
+    vector<vector<int>> temp(m,vector<int>(n,0));
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            int newValue=(i+j+k)
+        }
+    }
+}   
+int main()
+{
+    int k;
+    cin>>k;
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>> grid(m,vector<int>(n,0));
+
+    return 0;
+}
