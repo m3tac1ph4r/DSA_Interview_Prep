ab59b7838b309b69dd9dc44b8fba4a4bf1abd11f
Ashutosh Gupta <Ashutoshg547@gmail.com>
2022-10-31 14:40:58 +0530

    Time: 388 ms (39.89%), Space: 41.3 MB (51.01%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,50 +1,61 @@
 class Solution {
 public:
-    bool isSafe(int n, int m, int x, int y){
-        return x>=0 && x<n && y>=0 && y<m;
-    }
-    
-    int shortestPath(vector<vector<int>>& grid, int k) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
-        vector<vector<vector<bool>>> visited(n,vector<vector<bool>>(m,vector<bool>(k+1)));
-        
+    vector<int> delRow={-1,0,1,0};
+    vector<int> delCol={0,1,0,-1};
+    int solve(int n,int m,int k,vector<vector<int>>& grid)
+    {
+        vector<vector<vector<bool>>> visited(n,vector<vector<bool>>(m,vector<bool>(k+1,false)));
         queue<vector<int>> q;
-        q.push({k,0,0});
+        q.push({0,0,k});
         visited[0][0][k]=true;
         int res=0;
         
-        while(!q.empty()){
+        while(!q.empty())
+        {
             int size=q.size();
-            while(size--){
-                auto curr=q.front();
+            while(size--)
+            {
+                int row=q.front()[0];
+                int col=q.front()[1];
+                int kk=q.front()[2];
+                
                 q.pop();
-                int K=curr[0];
-                int x=curr[1];
-                int y=curr[2];
-                if(x==n-1 && y==m-1){
+                
+                if(row==n-1 and col==m-1)
                     return res;
-                }
-                for(int i=0;i<4;i++){
-                    int newX=x+dx[i];
-                    int newY=y+dy[i];
-                    if(isSafe(n,m,newX,newY)==false){
-                        continue;
-                    }
-                    if(grid[newX][newY]==1 && K>0 && visited[newX][newY][K-1]==false){ // if obstacle, use K
-                        visited[newX][newY][K-1]=true;
-                        q.push({K-1,newX,newY});
-                    }
-                    if(!visited[newX][newY][K] && grid[newX][newY]==0){
-                        visited[newX][newY][K]=true;
-                        q.push({K,newX,newY});
+                
+                for(int i=0;i<4;i++)
+                {
+                    int newRow=row+delRow[i];
+                    int newCol=col+delCol[i];
+                    
+                    if(newRow>=0 and newRow<n and newCol>=0 and newCol<m)
+                    {
+                        if(grid[newRow][newCol]==1 and kk>0 
+                           and visited[newRow][newCol][kk-1]==false)
+                        {
+                            q.push({newRow,newCol,kk-1});
+                            visited[newRow][newCol][kk-1]=true;
+                        }
+                        if(grid[newRow][newCol]==0 
+                                and visited[newRow][newCol][kk]==false)
+                        {
+                            q.push({newRow,newCol,kk});
+                            visited[newRow][newCol][kk]=true;
+                        }
                     }
                 }
             }
             res++;
         }
         return -1;
+    }
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        
+        int n=grid.size();
+        int m=grid[0].size();
+        
+        return solve(n,m,k,grid);
+        
     }
 };
