c93da1c3035a283bce063970a8b5d9738e53746f
Ashutosh Gupta <Ashutoshg547@gmail.com>
2022-11-20 23:58:46 +0530

    Added solution - LeetHub

--- LCM Triplet - GFG/lcm-triplet.cpp
+++ LCM Triplet - GFG/lcm-triplet.cpp
@@ -18,11 +18,11 @@ class Solution {
             return 6;
         if(N<=2)
             return N;
-        else if(N%2==1)
+        else if(N%2==1) //odd
             return (N*(N-1)*(N-2));
-        else if(N%3==0)
+        else if(N%3==0) // divisible by 3 and 2
             return ((N-1)*(N-2)*(N-3));
-        else if(N%2==0)
+        else if(N%2==0) // even
             return (N*(N-1)*(N-3));
     }
 };
